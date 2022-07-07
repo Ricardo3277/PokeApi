403c78e60eeb8a5c605526c0134e472c18a5cffc
Ricardo Montoya <loquendo3277@gmail.com>
2022-07-07 14:17:29 -0500

    Style: Styled the search bar as well as a background redesign

--- src/componets/Searchbar.js
+++ src/componets/Searchbar.js
@@ -4,24 +4,25 @@ const {useState} =  React;
 
 const Searchbar = () => {
     const [search, setSearch] = useState('');
+    const [setPokemon] =  useState();
 
     const onChange = (e) => {
         setSearch(e.target.value);
     }
     const onClick = async (e) =>{
         const data = await searchPokemon(search);
-        console.log(data);
+        setPokemon(data);
     }
 
     return (
-        <div>
-            <div>
+        <div className="searchbar-container">
+            <div className="searchbar">
                 <input placeholder="Buscar pokemon..."
                 onChange = {onChange}
 
                 />
             </div>
-            <div>
+            <div className="searchbar-btn">
                 <button onClick={onClick}>Buscar</button>
             </div>
         </div>
